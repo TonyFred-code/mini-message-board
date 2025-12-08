5f96c35b06d31ffd7a841a16f12c40accaa1d8dd
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-08 17:36:18 +0100

    feat: add handler for message details view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,20 +5,44 @@ const messages = [
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: 1,
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: 2,
   },
 ];
 
 const indexRouter = Router();
 
 indexRouter.use("/new", messageRouter);
+indexRouter.get("/message/:messageId", (req, res) => {
+  const messageId = req.params.messageId;
+
+  const message = messages.find((m) => m.id === Number(messageId));
+
+  if (!message) {
+    res.status(404).send("Message Not Found");
+    return;
+  }
+
+  res.render("message", {
+    user: message.user,
+    text: message.text,
+    added: message.added,
+  });
+});
+
 indexRouter.post("/new", (req, res) => {
   const { text, user } = req.body;
-  messages.push({ text: text, user: user, added: new Date() });
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date(),
+    id: messages.length,
+  });
   res.redirect("/");
 });
 
