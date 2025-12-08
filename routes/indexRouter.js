53be0023f3b32d61b535b6de2f9093a50ec0f864
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-08 17:11:25 +0100

    feat: add new message creation handler

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -16,6 +16,11 @@ const messages = [
 const indexRouter = Router();
 
 indexRouter.use("/new", messageRouter);
+indexRouter.post("/new", (req, res) => {
+  const { text, user } = req.body;
+  messages.push({ text: text, user: user, added: new Date() });
+  res.redirect("/");
+});
 
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages });
